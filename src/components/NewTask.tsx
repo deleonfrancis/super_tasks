86dee2464010fb4984e6b700d020707d47392595
Deleon Francis <dxfrancis.coding@gmail.com>
2022-04-04 15:48:13 -0400

    changed new task Priority options.

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -134,11 +134,9 @@ export const NewTask: React.FC<NewTaskProps> = (props) => {
             <MenuItem value={0}>
               <em>- None -</em>
             </MenuItem>
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={4}>4</MenuItem>
-            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={1}>Low</MenuItem>
+            <MenuItem value={2}>Medium</MenuItem>
+            <MenuItem value={3}>High</MenuItem>
           </Select>
         </FormControl>
       </div>
