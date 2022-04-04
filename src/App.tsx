1c5868c015d524b3937039ca2fc17aef05dfa51f
HDSTech <hswearine@live.com>
2022-04-04 10:05:35 -0400

    various changes to main app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,77 +2,178 @@ import React from "react";
 import "./App.css";
 import styled from "styled-components";
 import { SearchBar } from "./components/SearchBar";
-import { NewTask } from "./components/NewTask";
+import { TaskList } from "./components/TaskList";
+import { Task } from "./components/NewTask";
+import { TaskDetails } from "./components/TaskDetails";
+import moment from "moment";
+import { StoreProvider } from "./store/context";
+import { Buckets, TasksCompleted } from "./components/SideBar";
+import { bucketList } from "./components/Bucket";
 
-const StyledApp = styled.div`
-  background-color: #fff;
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  margin: 20px;
-  min-height: 300px;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
-    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
-  border-radius: 5px;
+export const list: Task[] = [
+	{
+		id: "1",
+		title: "Low priority task title",
+		details: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui dolore ipsum harum, unde molestias accusantium, nemo obcaecati earum quam ad porro neque nulla odit officia ab cumque adipisci commodi alias.
+    Nulla culpa ad pariatur reiciendis qui doloribus, sequi quas excepturi dolorum error libero inventore. Odio, praesentium? Esse nemo recusandae, rerum, facilis quasi repellat eos nihil deleniti quibusdam necessitatibus, ea odit?`,
+		priority: 1,
+		bucket: "vacation-id",
+		dueDate: "2020-01-01",
+		dateCreated: "2020-01-01",
+		isCompleted: false,
+		completedOn: null,
+	},
+	{
+		id: "2",
+		title: "Medium priority task title",
+		details: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem adipisci impedit expedita temporibus eligendi omnis. Doloremque obcaecati vero corrupti dicta accusantium ab quos possimus ducimus commodi qui. Non, delectus aspernatur?`,
+		priority: 2,
+		bucket: "p-team",
+		dueDate: "2020-01-01",
+		dateCreated: "2020-01-01",
+		isCompleted: false,
+		completedOn: null,
+	},
+	{
+		id: "3",
+		title: "High priority task title",
+		details: "Task 3 details",
+		priority: 3,
+		bucket: "vacation-id",
+		dueDate: "2020-01-01",
+		dateCreated: "2020-01-01",
+		isCompleted: false,
+		completedOn: null,
+	},
+	{
+		id: "4",
+		title: "No priority task title",
+		details: "Task 4 details",
+		priority: 0,
+		bucket: "initial",
+		dueDate: null,
+		dateCreated: "2020-01-01",
+		isCompleted: false,
+		completedOn: null,
+	},
+];
+
+const StyledApp = styled.div<{ isDetailsOpen: boolean }>`
+	font-family: "Noto Sans", sans-serif;
+	background-color: #fff;
+	display: flex;
+	flex-direction: column;
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	margin: 20px;
+	min-height: 300px;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+	border-radius: 5px;
+	margin: 50px auto;
+	width: 80%;
 `;
 
 const StyledSearchBar = styled.div`
-  display: flex;
-  flex: 1;
-  background-color: #e7725a;
-  width: 100%;
-  border-radius: 5px 5px 0 0;
+	display: flex;
+	flex: 1;
+	background-color: #e7725a;
+	width: 100%;
+	border-radius: 5px 5px 0 0;
 `;
 
 const Content = styled.div`
-  display: flex;
-  flex: 1;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  min-height: 300px;
+	display: flex;
+	flex: 1;
+	width: 100%;
+	justify-content: center;
+	align-items: stretch;
+	min-height: 300px;
 `;
 
 const FirstColumn = styled.div`
-  display: flex;
-  padding: 10px;
-  min-height: 300px;
-  flex: 1;
-  background-color: #f5f5f5;
-  border-radius: 0px 0px 0px 5px;
+	display: flex;
+	flex-direction: column;
+	padding: 10px;
+	min-height: 300px;
+	width: 300px;
+	background-color: #f5f5f5;
+	border-radius: 0px 0px 0px 5px;
 `;
 
 const SecondColumn = styled.div`
-  display: flex;
-  padding: 10px;
-  flex: 4;
-  min-height: 300px;
-  background-color: #fff;
-  border-radius: 0px 5px 5px 0px;
+	display: flex;
+	padding: 10px;
+	flex: 2;
+	min-height: 300px;
+	background-color: #fff;
+	border-radius: 0px 5px 5px 0px;
 `;
 
 const ThirdColumn = styled.div`
-  display: flex;
-  min-height: 300px;
-  background-color: #e8e8e8;
+	display: flex;
+	flex: 3;
+	padding: 10px;
+	min-height: 300px;
+	border-left: 1px solid #eeeeee;
+	border-radius: 0px 0px 5px 0px;
 `;
 
 function App(): JSX.Element {
-  return (
-    <StyledApp>
-      <StyledSearchBar>
-        <SearchBar />
-      </StyledSearchBar>
-      <Content>
-        <FirstColumn />
-        <SecondColumn>
-          <NewTask />
-        </SecondColumn>
-      </Content>
-    </StyledApp>
-  );
+	const [selectedTask, setSelectedTask] = React.useState<Task | null>(null);
+	const [taskList, setTaskList] = React.useState<Task[]>(list);
+
+	const handleSelectedTask = (task: Task): void => {
+		setSelectedTask(task);
+	};
+
+	const handleCompletedTask = (task: Task): void => {
+		setTaskList(
+			taskList.map((t) => {
+				if (t.id === task.id) {
+					t.isCompleted = !t.isCompleted;
+					t.completedOn = moment().format();
+				}
+				return t;
+			})
+		);
+	};
+
+	const value = {
+		taskList: list,
+		bucketList: bucketList,
+		selectedTask,
+		onDeleteTask: (task: Task) => {},
+	};
+
+	return (
+		<StoreProvider value={value}>
+			<StyledApp isDetailsOpen={!!selectedTask}>
+				<StyledSearchBar>
+					<SearchBar />
+				</StyledSearchBar>
+				<Content>
+					<FirstColumn>
+						<TasksCompleted />
+						<Buckets />
+					</FirstColumn>
+					<SecondColumn>
+						<TaskList
+							tasks={list}
+							bucket="vacation-id"
+							onSelectedTask={handleSelectedTask}
+							onCompletedTask={handleCompletedTask}
+						/>
+					</SecondColumn>
+					{selectedTask && (
+						<ThirdColumn>
+							<TaskDetails />
+						</ThirdColumn>
+					)}
+				</Content>
+			</StyledApp>
+		</StoreProvider>
+	);
 }
 
 export default App;
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -2,56 +2,72 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
-export interface NewTaskProps {
-  myName?: string;
-  age?: number;
-  isMarried?: boolean;
-}
+export type Task = {
+	id: string;
+	title: string;
+	details?: string;
+	priority: number;
+	bucket?: string | null;
+	dueDate?: string | null;
+	dateCreated?: string;
+	isCompleted: boolean;
+	completedOn: string | null;
+};
 
-export const NewTask: React.FC<NewTaskProps> = () => {
-  const [taskTitle, setTaskTitle] = useState<string>();
-  const [taskDetails, setTaskDetails] = useState<string>();
-  //   const [dateAndTime, setDateAndTime] = useState<string>("")
+//Task
+/**
+ * title: string
+ * details: string
+ * priority: number
+ * bucket: string
+ * dueDate: string
+ * isCompleted: boolean
+ */
 
-  const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTaskTitle(event.target.value);
-  };
-  const handleChangeDetails = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTaskDetails(event.target.value);
-  };
+export const NewTask: React.FC<Task> = () => {
+	const [taskTitle, setTaskTitle] = useState<string>();
+	const [taskDetails, setTaskDetails] = useState<string>();
+	//   const [dateAndTime, setDateAndTime] = useState<string>("")
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          id="standard-textarea"
-          label="Title"
-          placeholder="Type task title"
-          multiline
-          variant="standard"
-          value={taskTitle}
-          onChange={handleChangeTitle}
-        />
-      </div>
-      <div>
-        <TextField
-          id="standard-multiline-static"
-          label="Details"
-          multiline
-          rows={4}
-          defaultValue=""
-          variant="standard"
-          value={taskDetails}
-          onChange={handleChangeDetails}
-        />
-      </div>
-    </Box>
-  );
+	const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setTaskTitle(event.target.value);
+	};
+	const handleChangeDetails = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setTaskDetails(event.target.value);
+	};
+
+	return (
+		<Box
+			component="form"
+			sx={{
+				"& .MuiTextField-root": { m: 1, width: "25ch" },
+			}}
+			noValidate
+			autoComplete="off"
+		>
+			<div>
+				<TextField
+					id="standard-textarea"
+					label="Title"
+					placeholder="Type task title"
+					multiline
+					variant="standard"
+					value={taskTitle}
+					onChange={handleChangeTitle}
+				/>
+			</div>
+			<div>
+				<TextField
+					id="standard-multiline-static"
+					label="Details"
+					multiline
+					rows={4}
+					defaultValue=""
+					variant="standard"
+					value={taskDetails}
+					onChange={handleChangeDetails}
+				/>
+			</div>
+		</Box>
+	);
 };
